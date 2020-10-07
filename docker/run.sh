7e85da87d9fe41c906bcd897b64ab81a8e915349
Emily Zhou <zhouxemily@gmail.com>
2020-10-06 19:01:27 -0700

    Remove vision_project_interaction service
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -6,18 +6,12 @@ cd $FILE_DIR
 
 if [ -z ${1+x} ];
 then
-        SERVICE_NAME="vision_project_dev";
+        MODE="popup";
 else
-        SERVICE_NAME=$1;
+        MODE=$1;
 fi
 
-if [ -z ${2+x} ];
-then
-	MODE="popup";
-else
-	MODE=$2;
-fi
-
+SERVICE_NAME="vision_project_dev"
 ROS_MASTER_URI=$ROS_MASTER_URI docker-compose up --build
 case "$MODE" in
 	terminal | terminal_debug | t )
