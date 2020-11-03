98960d153b0b7be54c9431996eb5ae8cbf2ebbe8
Emily Zhou <zhouxemily@gmail.com>
2020-11-03 07:18:45 -0800

    Move functions to build states from JSON to lower-level class
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- ros-vision-interaction/scripts/demo-interaction/sar_demo.py
+++ ros-vision-interaction/scripts/demo-interaction/sar_demo.py
@@ -31,16 +31,16 @@ class DemoInteraction:
         self._jokes = interaction_setup_dict["jokes"]
         self._news_stories = interaction_setup_dict["news stories"]
 
-        states = self._build_states_from_dict(
+        states_from_json = self._add_joke_and_news_content_to_states_dict(
             interaction_setup_dict["states"],
             interaction_setup_dict["joke prefaces"]
         )
-        state_names = [state.name for state in states]
+        state_names = [str(state_name) for state_name in states_from_json.keys()]
 
         self._state_collection = StateCollection(
             name="demo interaction",
             init_state_name="ask to chat",
-            states=states
+            states=states_from_json
         )
         self._database = Database(
             database_file_name=database_file_name,
@@ -71,66 +71,22 @@ class DemoInteraction:
         self._is_start_interaction = False
         self._sleep_publisher.publish(True)
 
-    def _build_states_from_dict(self, states_from_json, joke_prefaces):
-        states = []
+    def _add_joke_and_news_content_to_states_dict(self, states_from_json, joke_prefaces):
         state_names = states_from_json.keys()
         for state_name in state_names:
-            name = str(state_name)
-            message_type = str(states_from_json[state_name]["message type"])
-            content = [str(content) for content in states_from_json[state_name]["content"]]
-            next_states = [str(next_state) for next_state in states_from_json[state_name]["next states"]]
-            transitions = self._make_transitions_from_json(states_from_json[state_name]["transitions"])
+            if "joke" in str(states_from_json[state_name]):
+                states_from_json[state_name]["content"] = self._make_random_joke_content(joke_prefaces)
+            if str(states_from_json[state_name]) == "share news story":
+                states_from_json[state_name]["content"] = str(random.choice(self._news_stories))
+        return states_from_json
 
-            try:
-                database_key_to_write = str(states_from_json[state_name]["database key to write"])
-            except KeyError:
-                database_key_to_write = None
-            try:
-                database_keys_to_read = [
-                    str(key_to_read) for key_to_read in states_from_json[state_name]["database keys to read"]
-                ]
-            except KeyError:
-                database_keys_to_read = None
-
-            new_state = State(
-                name=name,
-                message_type=message_type,
-                content=content,
-                next_states=next_states,
-                transitions=transitions,
-                database_key_to_write=database_key_to_write,
-                database_keys_to_read=database_keys_to_read
-            )
-
-            if "joke" in new_state.name:
-                new_state = self._make_random_joke_content(new_state, joke_prefaces)
-            if new_state.name == "share news story":
-                new_state = self._make_news_story(new_state)
-
-            states.append(new_state)
-        return states
-
-    def _make_transitions_from_json(self, json_transitions):
-        keys = [str(key) for key in json_transitions.keys()]
-        values = [str(value) for value in json_transitions.values()]
-        transitions = {}
-        for i in range(len(keys)):
-            transitions[keys[i]] = values[i]
-
-        return transitions
-
-    def _make_random_joke_content(self, joke_state, joke_prefaces):
+    def _make_random_joke_content(self, joke_prefaces):
         joke_category = str(random.choice(self._jokes.keys()))
         joke_content = str(random.choice(joke_prefaces).format(category=joke_category))
         joke = str(random.choice(self._jokes[joke_category]))
         joke_content += " <break time=\"1s\"/>" + joke
         self._jokes[joke_category].remove(joke)
-        joke_state.message.content = joke_content
-        return joke_state
-
-    def _make_news_story(self, news_story_state):
-        news_story_state.message.content = str(random.choice(self._news_stories))
-        return news_story_state
+        return joke_content
 
     def _screen_tap_listener_callback(self, _):
         self._is_start_interaction = True
