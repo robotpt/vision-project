0dbed48b4f9ddcab0c9a4228aaf4ed34f3313900
Emily Zhou <zhouxemily@gmail.com>
2021-01-21 12:20:09 -0800

    Remove outdated demo interaction files
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- src/ros_vision_interaction/src/vision-interaction/vision_interaction/controllers/__init__.py
+++ src/ros_vision_interaction/src/vision-interaction/vision_interaction/controllers/__init__.py
@@ -1,3 +1,2 @@
-from controllers.demo_interaction_controller import DemoInteractionController
 from controllers.interaction_manager import InteractionManager
 from controllers.vision_project_delegator import VisionProjectDelegator
