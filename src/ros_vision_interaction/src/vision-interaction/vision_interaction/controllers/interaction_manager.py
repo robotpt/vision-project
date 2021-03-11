22bbea7eeb2ec845654b73c25891f17d27b21546
Emily Zhou <zhouxemily@gmail.com>
2021-03-11 08:20:20 -0800

    Create planner before interaction is run instead of declaring it as a class variable
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- src/ros_vision_interaction/src/vision-interaction/vision_interaction/controllers/interaction_manager.py
+++ src/ros_vision_interaction/src/vision-interaction/vision_interaction/controllers/interaction_manager.py
@@ -45,7 +45,6 @@ class InteractionManager:
             interface = TerminalClientAndServerInterface(database=self._state_database)
         self._interface = interface
 
-        self._planner = MessagerPlanner(self._interaction_builder.possible_graphs)
         self._current_node_name = None
         self._current_interaction_type = None
 
@@ -53,16 +52,16 @@ class InteractionManager:
         if interaction_type not in Interactions.POSSIBLE_INTERACTIONS:
             raise ValueError("Not a valid interaction type")
 
-        self.build_interaction(interaction_type)
-        self.run_engine_once()
+        planner = self.build_interaction(interaction_type)
+        self.run_engine_once(planner)
 
-    def run_engine_once(self):
+    def run_engine_once(self, planner):
         engine = InteractionEngine(
             self._interface,
-            self._planner,
+            planner,
             self._interaction_builder.possible_graphs
         )
-        for node_name in engine.modified_run(self._planner):
+        for node_name in engine.modified_run(planner):
             self._current_node_name = node_name
 
     def build_interaction(self, interaction_type):
@@ -78,62 +77,68 @@ class InteractionManager:
         return build_interaction_dict[interaction_type]()
 
     def _build_first_interaction(self):
+        planner = MessagerPlanner(self._interaction_builder.possible_graphs)
         logging.info("Building first interaction")
-        self._planner.insert(
+        planner.insert(
             self._interaction_builder.interactions[InteractionBuilder.Graphs.FIRST_CHECKIN],
             post_hook=self._set_vars_after_first_interaction
         )
-        self._planner.insert(
+        planner.insert(
             self._interaction_builder.interactions[InteractionBuilder.Graphs.SCHEDULE_NEXT_CHECKIN],
             post_hook=self._state_database.set("is interaction finished", True)
         )
-        return self._planner
+        return planner
 
     def _build_ask_to_do_scheduled(self):
+        planner = MessagerPlanner(self._interaction_builder.possible_graphs)
         logging.info("Building ask to do scheduled")
-        self._planner.insert(self._interaction_builder.interactions[InteractionBuilder.Graphs.GREETING])
-        self._planner.insert(
+        planner.insert(self._interaction_builder.interactions[InteractionBuilder.Graphs.GREETING])
+        planner.insert(
             self._interaction_builder.interactions[InteractionBuilder.Graphs.ASK_TO_DO_SCHEDULED],
             post_hook=self._set_is_off_checkin
         )
-        return self._planner
+        return planner
 
     def _build_prompted_interaction(self):
+        planner = MessagerPlanner(self._interaction_builder.possible_graphs)
         logging.info("Building prompted interaction")
         if self._state_database.get("is done evaluation today"):
-            self._planner.insert(
+            planner.insert(
                 self._interaction_builder.interactions[InteractionBuilder.Graphs.GREETING]
             )
-        self._planner.insert(
+        planner.insert(
             self._interaction_builder.interactions[InteractionBuilder.Graphs.PROMPTED_CHECKIN],
             post_hook=self._state_database.set("is interaction finished", True)
         )
-        return self._planner
+        return planner
 
     def _build_scheduled_interaction(self):
+        planner = MessagerPlanner(self._interaction_builder.possible_graphs)
         logging.info("Building scheduled interaction")
-        self._planner.insert(self._interaction_builder.interactions[InteractionBuilder.Graphs.GREETING])
-        self._planner.insert(
+        planner.insert(self._interaction_builder.interactions[InteractionBuilder.Graphs.GREETING])
+        planner.insert(
             self._interaction_builder.interactions[InteractionBuilder.Graphs.SCHEDULED_CHECKIN],
             post_hook=self._set_vars_after_scheduled
         )
-        return self._planner
+        return planner
 
     def _build_reading_evaluation(self):
+        planner = MessagerPlanner(self._interaction_builder.possible_graphs)
         logging.info("Building reading evaluation")
-        self._planner.insert(
+        planner.insert(
             plan=self._interaction_builder.interactions[InteractionBuilder.Graphs.EVALUATION],
             post_hook=self._set_vars_after_evaluation
         )
-        return self._planner
+        return planner
 
     def _build_too_many_checkins(self):
+        planner = MessagerPlanner(self._interaction_builder.possible_graphs)
         logging.info("Building checkin limit reminder")
-        self._planner.insert(
+        planner.insert(
             plan=self._interaction_builder.interactions[InteractionBuilder.Graphs.TOO_MANY_CHECKINS],
             post_hook=self._state_database.set("is interaction finished", True)
         )
-        return self._planner
+        return planner
 
     def _set_vars_after_interaction(self):
         self._state_database.set("is interaction finished", True)
