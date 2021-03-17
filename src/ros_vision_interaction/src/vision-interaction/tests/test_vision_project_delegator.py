0f744ff664c19b7cbfc43325203fcd2d9a642c8f
Emily Zhou <zhouxemily@gmail.com>
2021-03-16 19:01:55 -0700

    Use helper methods to determine state and update tests
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- src/ros_vision_interaction/src/vision-interaction/tests/test_vision_project_delegator.py
+++ src/ros_vision_interaction/src/vision-interaction/tests/test_vision_project_delegator.py
@@ -9,7 +9,9 @@ from controllers import VisionProjectDelegator
 @pytest.fixture
 def vision_project_delegator(statedb):
     return VisionProjectDelegator(
-        statedb
+        statedb,
+        update_window_seconds=15,
+        minutes_between_interactions=1
     )
 
 
@@ -24,8 +26,8 @@ def test_determine_scheduled_interaction_not_prompted(vision_project_delegator, 
     statedb.set("first interaction datetime", datetime.datetime(2021, 1, 10, 6, 0, 0))
     statedb.set("last interaction datetime", datetime.datetime(2021, 1, 10, 6, 0, 0))
     statedb.set("is prompted by user", False)
-    statedb.set("is off checkin", False)
     statedb.set("is run prompted content", False)
+    statedb.set("is done eval today", False)
     next_checkin_time = datetime.datetime(2021, 1, 12, 12, 0, 0, 0)
     statedb.set("next checkin datetime", next_checkin_time)
 
@@ -36,7 +38,6 @@ def test_determine_scheduled_interaction_not_prompted(vision_project_delegator, 
         "2021-1-12 11:59:45",
         "2021-1-12 12:00:15",
     ]
-
     # check that scheduled is called when in time window
     for valid_time in scheduled_window_times:
         with freezegun.freeze_time(valid_time):
@@ -61,8 +62,7 @@ def test_determine_interaction_prompted_no_evaluation(vision_project_delegator, 
     next_checkin_time = datetime.datetime(2021, 1, 12, 12, 0, 0, 0)
     statedb.set("next checkin datetime", next_checkin_time)
     statedb.set("is prompted by user", True)
-    statedb.set("is done evaluation today", False)
-    statedb.set("is off checkin", False)
+    statedb.set("is done eval today", False)
     statedb.set("is run prompted content", False)
     # check scheduled interaction is called when within scheduled interaction window and evaluation not done yet
     # window of +/- 15 minutes
@@ -86,7 +86,7 @@ def test_determine_interaction_prompted_no_evaluation(vision_project_delegator, 
     ]
     for valid_time in scheduled_window_times:
         with freezegun.freeze_time(valid_time):
-            assert vision_project_delegator.get_interaction_type() == "ask to do scheduled"
+            assert vision_project_delegator.get_interaction_type() == "ask to do evaluation"
 
 
 def test_determine_interaction_prompted_evaluation_done(vision_project_delegator, statedb):
@@ -95,10 +95,32 @@ def test_determine_interaction_prompted_evaluation_done(vision_project_delegator
     next_checkin_time = datetime.datetime(2021, 1, 12, 12, 0, 0, 0)
     statedb.set("next checkin datetime", next_checkin_time)
     statedb.set("is prompted by user", True)
-    statedb.set("is done evaluation today", True)
-    statedb.set("is off checkin", False)
+    statedb.set("is done eval today", True)
     statedb.set("is run prompted content", False)
+    # check that prompted interaction is called when evaluation has been done and QT is prompted
+    times = [
+        "2021-1-12 2:00:00",
+        "2021-1-12 6:30:00",
+        "2021-1-12 11:59:44",
+        "2021-1-12 11:59:45",
+        "2021-1-12 12:00:00",
+        "2021-1-12 12:00:15",
+        "2021-1-12 12:00:16"
+    ]
+    for time in times:
+        with freezegun.freeze_time(time):
+            assert vision_project_delegator.get_interaction_type() == "prompted interaction"
 
+
+def test_determine_off_checkin(vision_project_delegator, statedb):
+    statedb.set("first interaction datetime", datetime.datetime(2021, 1, 10, 6, 0, 0))
+    statedb.set("last interaction datetime", datetime.datetime(2021, 1, 10, 6, 0, 0))
+    statedb.set("is prompted by user", True)
+    statedb.set("is run prompted content", False)
+    statedb.set("is done eval today", False)
+    next_checkin_time = datetime.datetime(2021, 1, 12, 12, 0, 0, 0)
+    statedb.set("next checkin datetime", next_checkin_time)
+    # check evaluation content is run after QT is prompted and participant agrees to do evaluation
     outside_scheduled_window_times = [
         "2021-1-12 2:00:00",
         "2021-1-12 6:30:00",
@@ -109,54 +131,23 @@ def test_determine_interaction_prompted_evaluation_done(vision_project_delegator
     ]
     for time in outside_scheduled_window_times:
         with freezegun.freeze_time(time):
-            assert vision_project_delegator.get_interaction_type() == "prompted interaction"
-
-
-def test_determine_off_checkin(vision_project_delegator, statedb):
-    statedb.set("first interaction datetime", datetime.datetime(2021, 1, 10, 6, 0, 0))
-    statedb.set("last interaction datetime", datetime.datetime(2021, 1, 10, 6, 0, 0))
-    statedb.set("is prompted by user", False)
-    statedb.set("is off checkin", False)
-    statedb.set("is run prompted content", False)
-    next_checkin_time = datetime.datetime(2021, 1, 12, 12, 0, 0, 0)
-    statedb.set("next checkin datetime", next_checkin_time)
-
-    outside_scheduled_window_times = [
-        "2021-1-12 2:00:00",
-        "2021-1-12 6:30:00",
-        "2021-1-12 11:59:44",
-        "2021-1-12 11:59:45",
-        "2021-1-12 12:00:15",
-        "2021-1-12 12:00:16"
-    ]
-    # check off checkin called when not in time window but "is off checkin" is True
-    for invalid_time in outside_scheduled_window_times:
-        statedb.set("is off checkin", True)
-        with freezegun.freeze_time(invalid_time):
             assert vision_project_delegator._is_off_checkin()
-            assert not vision_project_delegator._is_off_checkin()
-    # check off checkin not called when "is off checkin" is False
-    statedb.set("is off checkin", False)
-    assert not vision_project_delegator._is_off_checkin()
 
 
 def test_is_done_reading_evaluation(vision_project_delegator, statedb):
-    statedb.set("first interaction datetime", datetime.datetime(2021, 1, 10, 6, 0, 0))
+    statedb.set("first interaction datetime", datetime.datetime(2021, 2, 9, 6, 0, 0))
     statedb.set("next checkin datetime", datetime.datetime(2021, 2, 11, 2, 0, 0))
-
-    with freezegun.freeze_time("2021-02-10"):
-        statedb.set("last update datetime", datetime.datetime(2021, 2, 10, 12, 0, 0))
-        assert not statedb.get("is done evaluation today")
-        statedb.set("is done evaluation today", True)
+    statedb.set("last update datetime", datetime.datetime(2021, 2, 10, 12, 0, 0))
+    statedb.set("num of days since last eval", 0)
 
     # check scheduled interaction is called when valid time and evaluation not done yet
     with freezegun.freeze_time("2021-02-11 2:00:00"):
         assert statedb.get("last update datetime") == datetime.datetime(2021, 2, 10, 12, 0, 0)
         vision_project_delegator.update()
-        assert not statedb.get("is done evaluation today")
+        assert not statedb.get("is done eval today")
         assert vision_project_delegator.get_interaction_type() == "scheduled interaction"
 
     # check no interaction is called when valid time but evaluation already done
         with freezegun.freeze_time("2021-02-11 2:00:00"):
-            statedb.set("is done evaluation today", True)
+            statedb.set("is done eval today", True)
             assert vision_project_delegator.get_interaction_type() is None
--- src/ros_vision_interaction/src/vision-interaction/vision_interaction/controllers/vision_project_delegator.py
+++ src/ros_vision_interaction/src/vision-interaction/vision_interaction/controllers/vision_project_delegator.py
@@ -31,38 +31,34 @@ class VisionProjectDelegator:
     def update(self):
         if not self._state_database.is_set("last update datetime"):
             self._state_database.set("last update datetime", datetime.datetime.now())
-        if datetime.datetime.now().date() > self._state_database.get("last update datetime").date():
-            self._state_database.set("is done evaluation today", False)
+        if self._is_new_day():
+            if not self._state_database.get("is done eval today"):
+                num_of_days_since_last_eval = self._state_database.get("num of days since last eval")
+                self._state_database.set("num of days since last eval", num_of_days_since_last_eval + 1)
+            else:
+                self._state_database.set("is done eval today", False)
         self._state_database.set("last update datetime", datetime.datetime.now())
+
+    def _is_new_day(self):
+        return datetime.datetime.now().date() > self._state_database.get("last update datetime").date()
 
     def get_interaction_type(self):
         logging.info("Determining interaction type")
 
         if self._is_first_interaction():
             interaction_type = Interactions.FIRST_INTERACTION
+        elif self._is_time_for_scheduled_interaction():
+            interaction_type = Interactions.SCHEDULED_INTERACTION
+        elif self._is_ask_to_do_evaluation():
+            interaction_type = Interactions.ASK_TO_DO_EVALUATION
+        elif self._is_run_prompted_interaction():
+            interaction_type = Interactions.PROMPTED_INTERACTION
+        elif self._is_run_prompted_content():
+            interaction_type = Interactions.PROMPTED_CONTENT
+        elif self._is_off_checkin():
+            interaction_type = Interactions.EVALUATION
         else:
-            if self._state_database.get("is prompted by user"):
-                if not self._state_database.get("is done evaluation today"):
-                    is_in_scheduled_window = self._is_in_window(
-                        self._state_database.get("next checkin datetime"),
-                        self._scheduled_window_minutes,
-                        datetime.datetime.now()
-                    )
-                    if is_in_scheduled_window:
-                        interaction_type = Interactions.SCHEDULED_INTERACTION
-                    else:
-                        interaction_type = Interactions.ASK_TO_DO_SCHEDULED
-                else:
-                    interaction_type = Interactions.PROMPTED_INTERACTION
-            else:
-                if self._is_off_checkin():
-                    interaction_type = Interactions.EVALUATION
-                elif self._state_database.get("is run prompted content"):
-                    interaction_type = Interactions.PROMPTED_INTERACTION
-                elif self._is_time_for_scheduled_interaction():
-                    interaction_type = Interactions.SCHEDULED_INTERACTION
-                else:
-                    interaction_type = None
+            interaction_type = None
         return interaction_type
 
     def _is_first_interaction(self):
@@ -72,7 +68,7 @@ class VisionProjectDelegator:
         if not self._state_database.get("next checkin datetime"):
             return False
 
-        if self._state_database.get("is done evaluation today"):
+        if self._state_database.get("is done eval today"):
             is_time = False
         else:
             is_in_update_window = self._is_in_window(
@@ -80,9 +76,40 @@ class VisionProjectDelegator:
                 self._update_window_seconds,
                 datetime.datetime.now()
             )
-            is_time = is_in_update_window
+            is_prompted_in_scheduled_window = self._is_in_window(
+                self._state_database.get("next checkin datetime"),
+                self._scheduled_window_minutes,
+                datetime.datetime.now()
+            )
+
+            is_time = is_in_update_window or \
+                (is_prompted_in_scheduled_window and not self._state_database.get("is done eval today") and
+                 self._state_database.get("is prompted by user"))
 
         return is_time
+
+    def _is_ask_to_do_evaluation(self):  # this graphs begins with 'ask to chat'
+        is_in_scheduled_window = self._is_in_window(
+            self._state_database.get("next checkin datetime"),
+            self._scheduled_window_minutes,
+            datetime.datetime.now()
+        )
+        return not self._state_database.get("is done eval today") and \
+            self._state_database.get("is prompted by user") and \
+            not is_in_scheduled_window
+
+    def _is_run_prompted_interaction(self):
+        return self._state_database.get("is prompted by user") and \
+            self._state_database.get("is done eval today")
+
+    def _is_run_prompted_content(self):
+        return self._state_database.get("is prompted by user") and \
+            self._state_database.get("is run prompted content")
+
+    def _is_off_checkin(self):
+        return self._state_database.get("is prompted by user") and \
+               not self._state_database.get("is run prompted content") and \
+               not self._state_database.get("is done eval today")
 
     def _is_in_window(self, center, window, time_to_check):
         start_time = center - window
@@ -93,9 +120,3 @@ class VisionProjectDelegator:
         keys = self._state_database.get_keys()
         for key in keys:
             self._state_database.set(key, None)
-
-    def _is_off_checkin(self):
-        is_off_checkin = self._state_database.get("is off checkin")
-        if is_off_checkin:
-            self._state_database.set("is off checkin", False)
-        return is_off_checkin
