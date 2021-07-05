235c1a82dde274dc3cfe299f69377d35eb0beed8
Emily Zhou <zhouxemily@gmail.com>
2021-07-05 14:28:57 -0700

    Create class for database keys
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- src/ros_vision_interaction/src/vision-interaction/controllers/vision_project_delegator.py
+++ src/ros_vision_interaction/src/vision-interaction/controllers/vision_project_delegator.py
@@ -7,54 +7,99 @@ from controllers.interaction_manager import Interactions
 
 logging.basicConfig(level=logging.INFO)
 
+
+class DatabaseKeys:
+    BEST_SCORE = "best score"
+    CURRENT_EVAL_SCORE = "current eval score"
+    CURRENT_READING_COLOR = "current reading color"
+    CURRENT_READING_ID = "current reading id"
+    DIFFICULTY_LEVEL = "difficulty level"
+    FEEDBACK_VIDEOS = "feedback videos"
+    FIRST_INTERACTION_DATETIME = "first interaction datetime"
+    GOOD_TO_CHAT = "good to chat"
+    GRIT_FEEDBACK_INDEX = "grit feedback index"
+    IS_CONTINUE_PERSEVERANCE = "is continue perseverance"
+    IS_DO_EVALUATION = "is do evaluation"
+    IS_DONE_EVAL_TODAY = "is done eval today"
+    IS_DONE_GOAL_SETTING_TODAY = "is done goal setting today"
+    IS_DONE_MINDFULNESS_TODAY = "is done mindfulness today"
+    IS_DONE_PERSEVERANCE_TODAY = "is done perseverance today"
+    IS_DONE_PROMPTED_TODAY = "is done prompted today"
+    IS_INTERACTION_FINISHED = "is interaction finished"
+    IS_NEW_DIFFICULTY_LEVEL = "is new difficulty level"
+    IS_OFF_CHECKIN = "is off checkin"
+    IS_PROMPTED_BY_USER = "is prompted by user"
+    IS_PUBLISHED_CHOICES_TODAY = "is published choices today"
+    IS_START_PERSEVERANCE = "is start perseverance"
+    IS_USED_MAGNIFIER_TODAY = "is used magnifier today"
+    LAST_5_EVAL_SCORES = "last 5 eval scores"
+    LAST_INTERACTION_DATETIME = "last interaction datetime"
+    LAST_UPDATE_DATETIME = "last update datetime"
+    LAST_SCORE = "last score"
+    FEELINGS_INDEX = "feelings index"
+    NEXT_CHECKIN_DATETIME = "next checkin datetime"
+    NUM_OF_DAYS_SINCE_LAST_EVAL = "num of days since last eval"
+    NUM_OF_DAYS_SINCE_LAST_GOAL_SETTING = "num of days since last goal setting"
+    NUM_OF_DAYS_SINCE_LAST_MINDFULNESS = "num of days since last mindfulness"
+    NUM_OF_DAYS_SINCE_LAST_PERSEVERANCE = "num of days since last perseverance"
+    NUM_OF_DAYS_SINCE_LAST_PROMPT = "num of days since last prompt"
+    NUM_OF_PROMPTED_TODAY = "num of prompted today"
+    PERSEVERANCE_COUNTER = "perseverance counter"
+    READING_EVAL_DATA = "reading eval data"
+    READING_EVAL_INDEX = "reading eval index"
+    READING_EVAL_TYPE = "reading eval type"
+    USER_NAME = "user name"
+    VIDEO_TO_PLAY = "video to play"
+
+
 INITIAL_STATE_DB = {
-    "best score": None,
-    "current eval score": 0,
-    "current reading color": None,
-    "current reading id": None,
-    "difficulty level": 1,
-    "feedback videos": {
+    DatabaseKeys.BEST_SCORE: None,
+    DatabaseKeys.CURRENT_EVAL_SCORE: 0,
+    DatabaseKeys.CURRENT_READING_COLOR: None,
+    DatabaseKeys.CURRENT_READING_ID: None,
+    DatabaseKeys.DIFFICULTY_LEVEL: 1,
+    DatabaseKeys.FEEDBACK_VIDEOS: {
         "video 1": "https://www.youtube.com/embed/4b33NTAuF5E",
         "video 2": "https://www.youtube.com/embed/JXeJANDKwDc",
         "video 3": "https://www.youtube.com/embed/uzkD5SeuwzM",
         "video 4": "https://www.youtube.com/embed/QqsLTNkzvaY",
         "no video": ""
     },
-    "first interaction datetime": None,
-    "good to chat": None,
-    "grit feedback index": 0,
-    "is continue perseverance": None,
-    "is do evaluation": None,
-    "is done eval today": False,
-    "is done prompted today": False,
-    "is done perseverance today": False,
-    "is done mindfulness today": False,
-    "is done goal setting today": False,
-    "is interaction finished": False,
-    "is new difficulty level": False,
-    "is off checkin": None,
-    "is prompted by user": False,
-    "is published choices today": False,
-    "is start perseverance": False,
-    "is used magnifier today": False,
-    "last 5 eval scores": [],
-    "last interaction datetime": None,
-    "last update datetime": None,
-    "last score": None,
-    "feelings index": None,
-    "next checkin datetime": None,
-    "num of days since last eval": 0,
-    "num of days since last prompt": 0,
-    "num of days since last perseverance": 0,
-    "num of days since last mindfulness": 0,
-    "num of days since last goal setting": 0,
-    "num of prompted today": 0,
-    "perseverance counter": 0,
-    "reading eval index": 0,
-    "reading eval data": [],
-    "reading eval type": None,
-    "user name": "",
-    "video to play": None
+    DatabaseKeys.FIRST_INTERACTION_DATETIME: None,
+    DatabaseKeys.GOOD_TO_CHAT: None,
+    DatabaseKeys.GRIT_FEEDBACK_INDEX: 0,
+    DatabaseKeys.IS_CONTINUE_PERSEVERANCE: None,
+    DatabaseKeys.IS_DO_EVALUATION: None,
+    DatabaseKeys.IS_DONE_EVAL_TODAY: False,
+    DatabaseKeys.IS_DONE_GOAL_SETTING_TODAY: False,
+    DatabaseKeys.IS_DONE_MINDFULNESS_TODAY: False,
+    DatabaseKeys.IS_DONE_PERSEVERANCE_TODAY: False,
+    DatabaseKeys.IS_DONE_PROMPTED_TODAY: False,
+    DatabaseKeys.IS_INTERACTION_FINISHED: False,
+    DatabaseKeys.IS_NEW_DIFFICULTY_LEVEL: False,
+    DatabaseKeys.IS_OFF_CHECKIN: None,
+    DatabaseKeys.IS_PROMPTED_BY_USER: False,
+    DatabaseKeys.IS_PUBLISHED_CHOICES_TODAY: False,
+    DatabaseKeys.IS_START_PERSEVERANCE: False,
+    DatabaseKeys.IS_USED_MAGNIFIER_TODAY: False,
+    DatabaseKeys.LAST_5_EVAL_SCORES: [],
+    DatabaseKeys.LAST_INTERACTION_DATETIME: None,
+    DatabaseKeys.LAST_UPDATE_DATETIME: None,
+    DatabaseKeys.LAST_SCORE: None,
+    DatabaseKeys.FEELINGS_INDEX: None,
+    DatabaseKeys.NEXT_CHECKIN_DATETIME: None,
+    DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_EVAL: 0,
+    DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_GOAL_SETTING: 0,
+    DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_MINDFULNESS: 0,
+    DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_PERSEVERANCE: 0,
+    DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_PROMPT: 0,
+    DatabaseKeys.NUM_OF_PROMPTED_TODAY: 0,
+    DatabaseKeys.PERSEVERANCE_COUNTER: 0,
+    DatabaseKeys.READING_EVAL_DATA: [],
+    DatabaseKeys.READING_EVAL_INDEX: 0,
+    DatabaseKeys.READING_EVAL_TYPE: None,
+    DatabaseKeys.USER_NAME: "",
+    DatabaseKeys.VIDEO_TO_PLAY: None
 }
 
 
--- src/ros_vision_interaction/src/vision-interaction/tests/test_interaction_manager.py
+++ src/ros_vision_interaction/src/vision-interaction/tests/test_interaction_manager.py
@@ -4,6 +4,8 @@ import freezegun
 import pytest
 
 from controllers import InteractionManager
+from controllers.interaction_manager import Interactions
+from controllers.vision_project_delegator import DatabaseKeys
 from vision_project_tools.vision_engine import VisionInteractionEngine as InteractionEngine
 
 
@@ -34,72 +36,72 @@ def interaction_manager(statedb, interaction_builder, monkeypatch):
 
 
 def test_run_first_interaction(interaction_manager, statedb):
-    assert not interaction_manager._state_database.is_set("first interaction")
-    interaction_manager.run_interaction_once("first interaction")
-    assert interaction_manager._state_database.is_set("first interaction datetime")
+    assert not interaction_manager._state_database.is_set(DatabaseKeys.FIRST_INTERACTION_DATETIME)
+    interaction_manager.run_interaction_once(Interactions.FIRST_INTERACTION)
+    assert interaction_manager._state_database.is_set(DatabaseKeys.FIRST_INTERACTION_DATETIME)
 
 
 def test_run_scheduled_interaction(interaction_manager, statedb):
     with freezegun.freeze_time("2021-02-10"):
-        assert not statedb.is_set("last interaction time")
-        interaction_manager.run_interaction_once("scheduled interaction")
-        assert statedb.is_set("is done eval today")
+        assert not statedb.is_set(DatabaseKeys.LAST_INTERACTION_DATETIME)
+        interaction_manager.run_interaction_once(Interactions.SCHEDULED_INTERACTION)
+        assert statedb.is_set(DatabaseKeys.IS_DONE_EVAL_TODAY)
 
 
 def test_run_reading_evaluation(interaction_manager, statedb):
-    current_eval_index = statedb.get("reading eval index")
-    statedb.set("good to chat", "Yes")
-    statedb.set("is do evaluation", "Yes")
-    interaction_manager.run_interaction_once("scheduled interaction")
-    assert statedb.is_set("is done eval today")
-    assert statedb.get("is interaction finished")
-    assert statedb.get("reading eval index") == current_eval_index + 1
+    current_eval_index = statedb.get(DatabaseKeys.READING_EVAL_INDEX)
+    statedb.set(DatabaseKeys.GOOD_TO_CHAT, "Yes")
+    statedb.set(DatabaseKeys.IS_DO_EVALUATION, "Yes")
+    interaction_manager.run_interaction_once(Interactions.SCHEDULED_INTERACTION)
+    assert statedb.is_set(DatabaseKeys.IS_DONE_EVAL_TODAY)
+    assert statedb.get(DatabaseKeys.IS_INTERACTION_FINISHED)
+    assert statedb.get(DatabaseKeys.READING_EVAL_INDEX) == current_eval_index + 1
 
 
 def test_run_prompted_interaction(interaction_manager, statedb):
     with freezegun.freeze_time("2021-02-10"):
-        statedb.set("good to chat", "Yes")
-        assert statedb.get("num of prompted today") == 0
-        interaction_manager.run_interaction_once("prompted interaction")
-        assert statedb.get("num of prompted today") == 1
+        statedb.set(DatabaseKeys.GOOD_TO_CHAT, "Yes")
+        assert statedb.get(DatabaseKeys.NUM_OF_PROMPTED_TODAY) == 0
+        interaction_manager.run_interaction_once(Interactions.PROMPTED_INTERACTION)
+        assert statedb.get(DatabaseKeys.NUM_OF_PROMPTED_TODAY) == 1
 
 
 def test_determine_is_do_goal_setting(interaction_manager, statedb):
     # less than a week after first interaction
     first_interaction_datetime = datetime.datetime(2021, 4, 1, 12, 0, 0)
-    statedb.set("first interaction datetime", first_interaction_datetime)
-    statedb.set("feelings index", 2)
-    statedb.set("num of days since last prompt", 3)
-    statedb.set("num of days since last perseverance", 3)
+    statedb.set(DatabaseKeys.FIRST_INTERACTION_DATETIME, first_interaction_datetime)
+    statedb.set(DatabaseKeys.FEELINGS_INDEX, 2)
+    statedb.set(DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_PROMPT, 3)
+    statedb.set(DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_PERSEVERANCE, 3)
     with freezegun.freeze_time("2021-04-05"):
         assert not interaction_manager._is_do_goal_setting()
 
     first_interaction_datetime = datetime.datetime(2021, 4, 1, 12, 0, 0)
-    statedb.set("first interaction datetime", first_interaction_datetime)
-    statedb.set("feelings index", 2)
-    statedb.set("num of days since last prompt", 3)
-    statedb.set("num of days since last perseverance", 3)
-    statedb.set("num of days since last goal setting", 6)
-    statedb.set("last 5 eval scores", [5, 5, 5, 5, 5])
-    statedb.set("current eval score", 4)
+    statedb.set(DatabaseKeys.FIRST_INTERACTION_DATETIME, first_interaction_datetime)
+    statedb.set(DatabaseKeys.FEELINGS_INDEX, 2)
+    statedb.set(DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_PROMPT, 3)
+    statedb.set(DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_PERSEVERANCE, 3)
+    statedb.set(DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_GOAL_SETTING, 6)
+    statedb.set(DatabaseKeys.LAST_5_EVAL_SCORES, [5, 5, 5, 5, 5])
+    statedb.set(DatabaseKeys.CURRENT_EVAL_SCORE, 4)
     with freezegun.freeze_time("2021-04-10"):
         assert not interaction_manager._is_do_goal_setting()
-        statedb.set("num of days since last goal setting", 7)
+        statedb.set(DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_GOAL_SETTING, 7)
         assert interaction_manager._is_do_goal_setting()
 
 
 def test_determine_is_do_mindfulness(interaction_manager, statedb):
     # less than a week after first interaction
     first_interaction_datetime = datetime.datetime(2021, 4, 1, 12, 0, 0)
-    statedb.set("first interaction datetime", first_interaction_datetime)
+    statedb.set(DatabaseKeys.FIRST_INTERACTION_DATETIME, first_interaction_datetime)
     with freezegun.freeze_time("2021-04-05"):
         assert not interaction_manager._is_do_mindfulness()
 
     first_interaction_datetime = datetime.datetime(2021, 4, 1, 12, 0, 0)
-    statedb.set("first interaction datetime", first_interaction_datetime)
-    statedb.set("feelings index", 2)
-    statedb.set("num of days since last mindfulness", 2)
-    statedb.set("last 5 eval scores", [5, 5, 5, 5, 5])
-    statedb.set("current eval score", 4)
+    statedb.set(DatabaseKeys.FIRST_INTERACTION_DATETIME, first_interaction_datetime)
+    statedb.set(DatabaseKeys.FEELINGS_INDEX, 2)
+    statedb.set(DatabaseKeys.NUM_OF_DAYS_SINCE_LAST_MINDFULNESS, 2)
+    statedb.set(DatabaseKeys.LAST_5_EVAL_SCORES, [5, 5, 5, 5, 5])
+    statedb.set(DatabaseKeys.CURRENT_EVAL_SCORE, 4)
     with freezegun.freeze_time("2021-04-10"):
         assert interaction_manager._is_do_mindfulness()
